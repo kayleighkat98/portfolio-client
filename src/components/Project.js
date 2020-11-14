9ca7efc9a5b9fc0c5b61a4864ca83ed46cfbfb9d
Kayleigh Young <64609499+kayleighkat98@users.noreply.github.com>
2020-11-14 14:35:38 -0600

    modularize projects component

--- src/components/Project.js
+++ src/components/Project.js
@@ -1,65 +1,24 @@
 import React, { Component } from 'react';
-import store  from '../store/store';
 import '../styles/dist/Project.css';
-const projects = store.Projects;
-const tech = store.Tech;
 
 class Project extends Component {
     render() {
+        const {project, techUsed} = this.props
         return(
-            <div className='project'>
-                {projects.map((project, i) => {
-                    let techUsed = []
-                    tech.map((item, i) => {
-                        if (item === tech[tech.length-1]){
-                            techUsed.push(item.name+'.')
-                            return (techUsed)
-                        }
-                        techUsed.push(item.name+', ')
-                        return (techUsed)
-                    });
-                    if (this.props.filterValue === null){
-                        return(
-                            <div key={i}>
-                                <h2>{project.name}</h2>
-                                <p className='p'>A {project.stack}-stack application</p>
-                                <button><a href={project.links.prod_url} target="_blank" rel="noreferrer">Visit</a></button>
-                                <button><a href={project.links.client_git} target="_blank" rel="noreferrer">Client Repository</a></button>
-                                <button><a href={project.links.server_git} target="_blank" rel="noreferrer">Server Repository</a></button>
-                                <h3>Tech Used:</h3>
-                                <p className='p'>{techUsed}</p>
+            <div>
+                <h2>{project.name}</h2>
+                <p className='p'>A {project.stack}-stack application</p>
+                <button><a href={project.links.prod_url} target="_blank" rel="noreferrer">Visit</a></button>
+                <button><a href={project.links.client_git} target="_blank" rel="noreferrer">Client Repository</a></button>
+                <button><a href={project.links.server_git} target="_blank" rel="noreferrer">Server Repository</a></button>
+                <h3>Tech Used:</h3>
+                <p className='p'>{techUsed}</p>
     {/* 
-                                <h3>Summary:</h3>
-                                <p className='p'>{project.text.summary}</p>
-    
-                                <h3>My Role</h3>
-                                <p className='p'>{project.text.role}</p> */}
-                            </div>
-                        )
-                    }else{
-                        if ( this.props.filterValue.includes(project.id)){
-                            return(
-                                <div key={i}>
-                                    <h2>{project.name}</h2>
-                                    <p className='p'>A {project.stack}-stack application</p>
-                                    <button><a href={project.links.prod_url} target="_blank" rel="noreferrer">Visit</a></button>
-                                    <button><a href={project.links.client_git} target="_blank" rel="noreferrer">Client Repository</a></button>
-                                    <button><a href={project.links.server_git} target="_blank" rel="noreferrer">Server Repository</a></button>
-                                    <h3>Tech Used:</h3>
-                                    <p className='p'>{techUsed}</p>
-        {/* 
-                                    <h3>Summary:</h3>
-                                    <p className='p'>{project.text.summary}</p>
-        
-                                    <h3>My Role</h3>
-                                <p className='p'>{project.text.role}</p> */}
-                                </div>
-                            )
-                        } else {
-                            return ('')
-                        }
-                    } 
-                } )}
+                <h3>Summary:</h3>
+                <p className='p'>{project.text.summary}</p>
+
+                <h3>My Role</h3>
+                <p className='p'>{project.text.role}</p> */}
             </div>
         );
     };
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,7 +1,9 @@
 import React, { Component } from 'react';
 import TechFilter from './TechFilter';
 import Project from './Project';
-
+import store  from '../store/store';
+const projects = store.Projects;
+const tech = store.Tech;
 class Projects extends Component {
     state = {
         filterValue: null
@@ -20,9 +22,38 @@ class Projects extends Component {
                     setFilter = {this.setFilter}
                     undoFilter = {this.undoFilter}
                 />
-                <Project
-                    filterValue = {this.state.filterValue}
-                />
+                {projects.map((project, i) => {
+                    let techUsed = []
+                    tech.map((item, i) => {
+                        if (item === tech[tech.length-1]){
+                            techUsed.push(item.name+'.')
+                            return (techUsed)
+                        }
+                        techUsed.push(item.name+', ')
+                        return (techUsed)
+                    });
+                    if (this.state.filterValue === null){
+                        return(
+                            <Project
+                                key = {i}
+                                project = {project}
+                                techUsed = {techUsed}
+                            />
+                        )
+                    }else{
+                        if ( this.state.filterValue.includes(project.id)){
+                            return(
+                                <Project
+                                    key = {i}
+                                    project = {project}
+                                    techUsed = {techUsed}
+                                />
+                            )
+                        } else {
+                            return ('')
+                        };
+                    };
+                })}
             </div>
         );
     };
