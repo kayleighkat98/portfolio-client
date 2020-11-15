11f277adf33bdc0180a33e1bcfff8caea774b0c6
Kayleigh Young <64609499+kayleighkat98@users.noreply.github.com>
2020-11-15 13:39:15 -0600

    start bio paragraph

--- src/components/Bio.js
+++ src/components/Bio.js
@@ -6,6 +6,16 @@ class Bio extends Component {
         return(
             <div className='container'>
                <h1>About me</h1>
+               <p>
+                   You are so lucky to have found me. 
+                   Chances are, your life just got a little bit better. 
+                   Hi, I'm Kayleigh Young, a full-stack web-developer based in Dallas, Texas.
+                </p>
+                <br/>
+                <p>
+                    If you need an excited engineer who can pick up on new concepts in the drop of a hat, or a cool coder who thinks outside the box- congratulations, your life just got better.
+                    I am one of those people who " just gets it " when it comes to programing.
+                </p>
             </div>
         );
     }
