c76c06f0518b65883732b17d270096826a85832a
Kayleigh Young <64609499+kayleighkat98@users.noreply.github.com>
2020-11-15 12:04:45 -0600

    add avatar for landing page

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
+import avatar from '../store/images/avatar.png'
 
 class Landing extends Component {
     render() {
         return(
             <div className='container landing'>
                <h1>Landing</h1>
+               <img className='avatar' src={avatar} alt='avatar' />
             </div>
         );
     };
