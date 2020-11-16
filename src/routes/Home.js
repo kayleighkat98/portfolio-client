b10d5e635cf66896d84433a9021d670be0f3e932
Kayleigh Young <64609499+kayleighkat98@users.noreply.github.com>
2020-11-15 18:08:36 -0600

    add scroll up arrow

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -3,7 +3,7 @@ import Landing from '../components/Landing';
 import Bio from '../components/Bio';
 import Contact from '../components/Contact';
 import Projects from '../components/Projects';
-
+import ScrollArrow from '../components/ScrollArrow.js';
 class Home extends Component {
     render() {
         return(
@@ -12,6 +12,7 @@ class Home extends Component {
                 <Bio/>
                 <Contact/>
                 <Projects/>
+                <ScrollArrow/>
             </div>
         );
     };
