0e5e182fcab1d093d12df3cd46540def72d0971f
Kayleigh Young <64609499+kayleighkat98@users.noreply.github.com>
2021-02-12 22:05:02 -0600

    add input fields

--- src/components/CreateProject.js
+++ src/components/CreateProject.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-
+import CreatableSelect from 'react-select/creatable';
 class CreateProject extends Component {
 
     constructor(props) {
@@ -12,7 +12,41 @@ class CreateProject extends Component {
     render() {
         return(
             <div className='login'>
-               <h1>New project</h1>
+                <h1>New project</h1>
+                <label id='name'>Name: </label>
+                <CreatableSelect/>
+                <label id='team'>Team Size: </label>
+                <CreatableSelect/>
+                <label id='stack'>Stack: </label>
+                <CreatableSelect/>
+                <section className='text'>
+                    <label id='summary'>Summary: </label>
+                    <CreatableSelect/>
+                    <label id='role'>Role: </label>
+                    <CreatableSelect/>
+                </section>
+                <section className='links'>
+                    <label>Production Url: </label>
+                    <CreatableSelect/>
+                    <label>Client Url: </label>
+                    <CreatableSelect/>
+                    <label>Server Url:</label>
+                    <CreatableSelect/>
+                </section>
+                <section className='images'>
+                    <label id='type'>Type: </label>
+                    <CreatableSelect/>
+                    <label id='label'>Label: </label>
+                    <CreatableSelect/>
+                    <label id='url'>Url: </label>
+                    <CreatableSelect/>
+                </section>
+                <section className='tech'>
+                    <label id='label'>Label: </label>
+                    <CreatableSelect/>
+                    <label id='stack'>Stack: </label>
+                    <CreatableSelect/>
+                </section>
             </div>
         );
     }
